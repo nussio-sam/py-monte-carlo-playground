4ac347cd52401d8318bc4cc022f093c935e7afd1
plasma-sock <snussio@keyschool.org>
2025-04-21 23:25:45 -0400

    Vectorized EuropeanOptions
    Implemented numpy arrays/vectors to reduce compute time of GBM function

--- simulations/european_options.py
+++ simulations/european_options.py
@@ -1,4 +1,4 @@
-from random import gauss as normal
+import numpy as np
 from math import e
 
 # The Geometric Brownian Motion is the first step towards modeling the full Black-Scholes Model
@@ -6,9 +6,9 @@ from math import e
 # This random motion, where the logarithm of the quantity follows Brownian Motion,
 # is used to simulate stock prices
 
-class european_options:
+class EuropeanOptions:
     
-    def __init__(self, strike_price: int, vol: int, rf_rate: int, time: int, init_price: int, time_steps = 1):
+    def __init__(self, strike_price: float, vol: float, rf_rate: float, time: float, init_price: float, time_steps = 1):
         
         #These are all the basic variables in the GBM equation for option pricing
         self.strike_price = strike_price
@@ -18,18 +18,18 @@ class european_options:
         self.init_price = init_price
         self.time_steps = time_steps
         
-    def GBM(self, sim_num):
-        # The discrete (analytic) form, the equation goes by timestep, solved through Itô Calculus
-        Z = []
-        for i in range(sim_num):
-            Z.append(normal(mu = 0.0, sigma = 1))
+    def GBM(self, sim_num: float):
+        # Simulates final stock prices using the analytic solution of GBM
         
-        #Initializing and iterating over the end result, which is a dimensional list the same size as our sim_num
-        #This list is the end of period stock pricings
-        ST = []
-        for rand in Z:
-            ST.append(self.init_price * e**(self.time * (self.rf_rate - (0.5 * self.vol ** 2)) + self.vol * self.time**0.5 * rand))
+        #Creating array of random normal numbers with sim_num size
+        Z = np.random.normal(0, 1, sim_num)
         
+        #Dividing up the GBM equation into manageable chunks of drift and diffusion
+        drift = self.time * (self.rf_rate - (0.5 * self.vol ** 2))
+        diff = self.vol * np.sqrt(self.time) * Z
+
+        #Simulate end-of-period stock prices (ST) based on normal samples
+        ST = self.init_price * np.exp(drift + diff)
         return ST
         
     def european_call(self, sim_num: int) -> float:
@@ -38,20 +38,19 @@ class european_options:
         #Using the GBM to simulate stock paths
         ST = self.GBM(sim_num)
         
-        #Simply finding if the payoff is worth it or not, placing 0 if not
-        payoffs = []
-        for end_price in ST:
-            payoffs.append(max(end_price - self.strike_price, 0))
+        #Calculating payoffs of end-of-period call through max of paths
+        payoffs = np.maximum(ST - self.strike_price, np.zeros_like(ST))
             
-        return e ** ( -1 * self.rf_rate * self.time) * (sum(payoffs)/len(payoffs))
+        #returning the discounted payoffs
+        return e ** ( -1 * self.rf_rate * self.time) * np.mean(payoffs)
     
     def european_put(self, sim_num: int) -> float:
-        #Also a function for european options, this checks for below strike price
+        #Also a function for american options, this checks for below strike price
         ST = self.GBM(sim_num)
         
-        payoffs = []
-        for end_price in ST:
-            payoffs.append(max(self.strike_price - end_price, 0))
+        payoffs = np.maximum(self.strike_price - ST, np.zeros_like(ST))
+            
+        return e ** (-1 * self.rf_rate * self.time) * np.mean(payoffs)
             
         return e ** (-1 * self.rf_rate * self.time) * (sum(payoffs)/len(payoffs))
 
